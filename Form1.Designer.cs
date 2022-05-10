697ff26fddc14039a04c1277938abe8cc70ba96b
Daniil Miromanov <miromanov.work@gmail.com>
2022-05-11 00:29:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace KoshiTask_lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.F_eps = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.Run_Runge_Kutta_3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.F_h = new System.Windows.Forms.TextBox();
+            this.F_t0 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // F_eps
+            // 
+            this.F_eps.AcceptsReturn = true;
+            this.F_eps.Location = new System.Drawing.Point(650, 58);
+            this.F_eps.Name = "F_eps";
+            this.F_eps.Size = new System.Drawing.Size(93, 20);
+            this.F_eps.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(594, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "eps = ";
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.Location = new System.Drawing.Point(12, 32);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0D;
+            this.zedGraphControl.ScrollMaxX = 0D;
+            this.zedGraphControl.ScrollMaxY = 0D;
+            this.zedGraphControl.ScrollMaxY2 = 0D;
+            this.zedGraphControl.ScrollMinX = 0D;
+            this.zedGraphControl.ScrollMinY = 0D;
+            this.zedGraphControl.ScrollMinY2 = 0D;
+            this.zedGraphControl.Size = new System.Drawing.Size(550, 550);
+            this.zedGraphControl.TabIndex = 3;
+            // 
+            // Run_Runge_Kutta_3
+            // 
+            this.Run_Runge_Kutta_3.Font = new System.Drawing.Font("Times New Roman", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Run_Runge_Kutta_3.Location = new System.Drawing.Point(620, 291);
+            this.Run_Runge_Kutta_3.Name = "Run_Runge_Kutta_3";
+            this.Run_Runge_Kutta_3.Size = new System.Drawing.Size(166, 48);
+            this.Run_Runge_Kutta_3.TabIndex = 4;
+            this.Run_Runge_Kutta_3.Text = "Run ";
+            this.Run_Runge_Kutta_3.UseVisualStyleBackColor = true;
+            this.Run_Runge_Kutta_3.Click += new System.EventHandler(this.Run_Runge_Kutta_3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(611, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "h = ";
+            // 
+            // F_h
+            // 
+            this.F_h.AcceptsReturn = true;
+            this.F_h.Location = new System.Drawing.Point(650, 100);
+            this.F_h.Name = "F_h";
+            this.F_h.Size = new System.Drawing.Size(93, 20);
+            this.F_h.TabIndex = 6;
+            // 
+            // F_t0
+            // 
+            this.F_t0.AcceptsReturn = true;
+            this.F_t0.Location = new System.Drawing.Point(650, 138);
+            this.F_t0.Name = "F_t0";
+            this.F_t0.Size = new System.Drawing.Size(93, 20);
+            this.F_t0.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 6.792453F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(618, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(611, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "t₀ = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 619);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.F_t0);
+            this.Controls.Add(this.F_h);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Run_Runge_Kutta_3);
+            this.Controls.Add(this.zedGraphControl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.F_eps);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox F_eps;
+        private System.Windows.Forms.Label label1;
+        private ZedGraph.ZedGraphControl zedGraphControl;
+        private System.Windows.Forms.Button Run_Runge_Kutta_3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox F_h;
+        private System.Windows.Forms.TextBox F_t0;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace KoshiTask_lab_4
+{
+    public partial class Form1 : Form
+    {
+        //Создаю массив координат концов кривых.
+        PointPairList end_point = new PointPairList();
+
+        double eps = 0.1;
+        double h = 0.002;
+        double t0 = 1;
+        string zedGraph_PointValueEvent(ZedGraphControl sender, GraphPane pane, CurveItem curve, int iPt)
+        {
+            // Получим точку, около которой находимся
+            PointPair point = curve[iPt];
+
+            //Результат наведения;
+            string result = "";
+            // Сформируем строку
+            result = string.Format("X: {0:F3}\nY: {1:F3}", point.X, point.Y);
+
+            if (point.X == 0 && point.Y == 1 || point.X == 0 && point.Y == -1 || point.X == 1 && point.Y == 0 || point.X == -3 && point.Y == 2)
+            {
+                result = string.Format("X: {0:F3}\nY: {1:F3}\nОсобая точка!", point.X, point.Y);
+            }
+
+            if (end_point.Contains(new PointPair(point.X, point.Y)))
+            {
+                result = string.Format("X: {0:F3}\nY: {1:F3}\nКонец кривой!", point.X, point.Y);
+            }
+
+            return result;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            // Включим показ всплывающих подсказок при наведении курсора на график
+            zedGraphControl.IsShowPointValues = true;
+            // Будем обрабатывать событие PointValueEvent, чтобы изменить формат представления координат
+            zedGraphControl.PointValueEvent += new ZedGraphControl.PointValueHandler(zedGraph_PointValueEvent);
+        }
+
+        double f1(double t, double x, double y) { 
+            return ((x + y) * (x + y) - 1);
+        }
+
+        double f2(double t, double x, double y) {
+            return (-(y * y) - x + 1);
+        }
+
+        PointPairList Runge_Kutta_Method_3(double x, double y)
+        {
+            PointPairList result_curve = new PointPairList();
+            double t;
+
+            result_curve.Add(x, y);
+
+            for (t = 0; t <= t0; t += h) {
+                //Расчет нужных коэффициентов для формулы. 
+                double Q1x = h * f1(t, x, y);
+                double Q1y = h * f2(t, x, y);
+
+                double Q2x = h * f1(t + h / 2, x + Q1x / 2, y + Q1y / 2);
+                double Q2y = h * f2(t + h / 2, x + Q1x / 2, y + Q1y / 2);
+
+                double Q3x = h * f1(t + h, x - Q1x + 2 * Q2x, y - Q1y + 2 * Q2y);
+                double Q3y = h * f2(t + h, x - Q1x + 2 * Q2x, y - Q1y + 2 * Q2y);
+
+                //Расчет новой точки. 
+                x += (Q1x + 4 * Q2x + Q3x) / 6;
+                y += (Q1y + 4 * Q2y + Q3y) / 6;
+
+                //Добавление полученной точки в список точек кривой. 
+                result_curve.Add(x, y);
+            }
+            return result_curve;
+        }
+
+        private void Draw()
+        {
+            GraphPane pane = zedGraphControl.GraphPane;
+            pane.Title.Text = "Метод Рунге-Кутта 3 порядка.";
+
+            //Меняем цвет рамки.
+            pane.Fill.Type = FillType.Solid;
+            pane.Fill.Color = Color.Silver;
+
+
+            zedGraphControl.PanButtons = MouseButtons.Left;
+            zedGraphControl.PanModifierKeys = Keys.None;
+
+            pane.CurveList.Clear();
+
+            //Создаю массив кривых, которые будут отображаться на графике. 
+            List<LineItem> myCurve = new List<LineItem> { };
+            //Создаю массив особых точек.
+            PointPairList specific_point = new PointPairList();
+            //Добавляю особые точки.
+            specific_point.Add(0, 1);
+            specific_point.Add(0, -1);
+            specific_point.Add(1, 0);
+            specific_point.Add(-3, 2);
+
+            //Вспомогательная переменная для того чтобы обращаться к кривым. 
+            int t = 0;
+
+            for (int i = 0; i < 4; i++) {
+                //Отрисовка кривых вокруг i-ой особой точки.
+                myCurve.Add(pane.AddCurve("", Runge_Kutta_Method_3(specific_point[i].X + eps, specific_point[i].Y), Color.Blue, SymbolType.Circle));
+                myCurve.Add(pane.AddCurve("", Runge_Kutta_Method_3(specific_point[i].X - eps, specific_point[i].Y), Color.Red, SymbolType.Circle));
+                myCurve.Add(pane.AddCurve("", Runge_Kutta_Method_3(specific_point[i].X, specific_point[i].Y + eps), Color.Green, SymbolType.Circle));
+                myCurve.Add(pane.AddCurve("", Runge_Kutta_Method_3(specific_point[i].X, specific_point[i].Y - eps), Color.Yellow, SymbolType.Circle));
+
+                myCurve[i + t].Line.IsVisible = false;            //Видимость линий между точками.
+                myCurve[i + t].Symbol.Fill.Color = Color.Blue;    //Цвет заполнения кружков.
+                myCurve[i + t].Symbol.Fill.Type = FillType.Solid; //Тип заполнения - сплошная заливка.
+                myCurve[i + t].Symbol.Size = 3;                   //Размер кружков.
+
+                // Добавляю концы кривой в массив концов кривых, для их отображения при наведении.
+                end_point.Add(myCurve[i + t][0].X, myCurve[i + t][0].Y);
+                end_point.Add(myCurve[i + t][myCurve[i + t].Points.Count - 1].X, myCurve[i + t][myCurve[i + t].Points.Count - 1].Y);
+
+                myCurve[i + t + 1].Line.IsVisible = false;
+                myCurve[i + t + 1].Symbol.Fill.Color = Color.Red;
+                myCurve[i + t + 1].Symbol.Fill.Type = FillType.Solid;
+                myCurve[i + t + 1].Symbol.Size = 3;
+
+                end_point.Add(myCurve[i + t + 1][0].X, myCurve[i + t + 1][0].Y);
+                end_point.Add(myCurve[i + t + 1][myCurve[i + t + 1].Points.Count - 1].X, myCurve[i + t + 1][myCurve[i + t + 1].Points.Count - 1].Y);
+
+                myCurve[i + t + 2].Line.IsVisible = false;
+                myCurve[i + t + 2].Symbol.Fill.Color = Color.Green;
+                myCurve[i + t + 2].Symbol.Fill.Type = FillType.Solid;
+                myCurve[i + t + 2].Symbol.Size = 3;
+
+                end_point.Add(myCurve[i + t + 2][0].X, myCurve[i + t + 2][0].Y);
+                end_point.Add(myCurve[i + t + 2][myCurve[i + t + 2].Points.Count - 1].X, myCurve[i + t + 2][myCurve[i + t + 2].Points.Count - 1].Y);
+
+                myCurve[i + t + 3].Line.IsVisible = false;
+                myCurve[i + t + 3].Symbol.Fill.Color = Color.Yellow;
+                myCurve[i + t + 3].Symbol.Fill.Type = FillType.Solid;
+                myCurve[i + t + 3].Symbol.Size = 3;
+
+                end_point.Add(myCurve[i + t + 3][0].X, myCurve[i + t + 3][0].Y);
+                end_point.Add(myCurve[i + t + 3][myCurve[i + t + 3].Points.Count - 1].X, myCurve[i + t + 3][myCurve[i + t + 3].Points.Count - 1].Y);
+
+                t += 3;
+            }
+
+            //Отрисовка особых точек. 
+            LineItem sp_points =  pane.AddCurve("",specific_point, Color.Chocolate, SymbolType.Circle);
+            sp_points.Line.IsVisible = false;
+            sp_points.Symbol.Fill.Color = Color.Chocolate;
+            sp_points.Symbol.Fill.Type = FillType.Solid;
+            sp_points.Symbol.Size = 6;
+
+            //Добавление линий сетки на график и окраска их в серый.
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.YAxis.MajorGrid.IsVisible = true;
+            pane.XAxis.MajorGrid.Color = Color.Black;
+            pane.YAxis.MajorGrid.Color = Color.Black;
+
+            // Ось X будет пересекаться с осью Y на уровне Y = 0
+            pane.XAxis.Cross = 0.0;
+
+            // Ось Y будет пересекаться с осью X на уровне X = 0
+            pane.YAxis.Cross = 0.0;
+
+            // Отключим отображение первых и последних меток по осям
+            pane.XAxis.Scale.IsSkipFirstLabel = true;
+            pane.XAxis.Scale.IsSkipLastLabel = true;
+
+            // Отключим отображение меток в точке пересечения с другой осью
+            pane.XAxis.Scale.IsSkipCrossLabel = true;
+
+            // Спрячем заголовки осей
+            pane.XAxis.Title.IsVisible = false;
+            pane.YAxis.Title.IsVisible = false;
+
+            // Обновляем график
+            zedGraphControl.Invalidate();
+        }
+
+        private void Run_Runge_Kutta_3_Click(object sender, EventArgs e)
+        {
+            if (F_eps.Text != "")
+            {
+                eps = Convert.ToDouble(F_eps.Text);
+            }
+            if (F_h.Text != "")
+            {
+                h = Convert.ToDouble(F_h.Text);
+            }
+            if (F_t0.Text != "")
+            {
+                t0 = Convert.ToDouble(F_t0.Text);
+            }
+            Draw();
+        }
+    }
+}
